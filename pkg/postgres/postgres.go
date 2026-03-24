610812864b9f57e29188b92b381b652ba7c3d382
Ablebil <hafizhfaiq07@gmail.com>
2026-03-24 12:20:50 +0700

    feat: add sslmode and timezone config on db initialization

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -11,7 +11,7 @@ import (
 )
 
 func StartPostgres() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
 		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
